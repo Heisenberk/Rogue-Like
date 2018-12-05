3945df14998b56174cdbf907ed188893f9597cd1
Mehdi Merimi <mehdi.78340@hotmail.fr>
2018-12-05 10:45:46 +0100

    fin de l'implementation de WorldTest

--- src/test/java/fr/uvsq/inf103/rogue_like/world/WorldTest.java
+++ src/test/java/fr/uvsq/inf103/rogue_like/world/WorldTest.java
@@ -5,37 +5,42 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Tests unitaires sur la classe World.
+ */
 public class WorldTest {
 
 	private	World w;
 	private Element[][] e;
-		  @Before 
-		  public void initialize() {
-			  e = new Element[50][70];
-		       w= new World(e);
-		  }
+	/**
+	 * Permet d'initialiser les variables que l'on va tester. 
+	 */
+	@Before 
+	public void initialize() {
+		e = new Element[50][70];
+		w= new World(e);
+	}
+	
+	/**
+	 * Tests l'affectation de l'enumeration FLOOR à un element.
+	 */
 	@Test
 	public void testAffectationElement() {
 		w.affectationElement(1,1,Element.FLOOR);
 		assertEquals(w.getPosition(1,1),Element.FLOOR);
 	}
 
-	@Test
-	public void testElementLengthAbscisse() {
-		int lenght=w.getElement().length;
-		assertEquals(lenght,50);
-	}
-	
-	@Test
-	public void TestElementLenghtOrdonne() {
-		int length=w.getElement()[0].length;
-		assertEquals(length,70);
-	}
-	
+	/**
+	 * Tests accesseur Width.
+	 */
 	@Test
 	public void testWidth() {
 		assertEquals(w.getWidth(),50);
 	}
+	
+	/**
+	 * Tests acceseur Height.
+	 */
 	@Test
 	public void testHeigth() {
 		assertEquals(w.getHeight(),70);
