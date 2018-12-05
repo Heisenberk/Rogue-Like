dffbfc775d31c6a4d11f469eae0564d9fb56a385
uvsq21501810 <clement.caumes@ens.uvsq.fr>
2018-12-05 12:22:02 +0100

    Javadoc terminee

--- src/main/java/fr/uvsq/inf103/rogue_like/world/World.java
+++ src/main/java/fr/uvsq/inf103/rogue_like/world/World.java
@@ -29,17 +29,27 @@ public class World {
     private int height;
 
     /**
-     * Accesseur
-     * @return
+     * Accesseur de la largeur de la map.
+     * @return largeur de la map.
      */
     public int getHeight() { return height; }
 
+    /**
+     * Constructeur de World.
+     * @param elements matrice representant le mode a creer.
+     */
     public World(Element[][] elements){
         this.element = elements;
         this.width = elements.length;
         this.height = elements[0].length;
     }
-    
+
+    /**
+     * Methode retournant la nature de la case en (x,y).
+     * @param x abscisse de la case.
+     * @param y ordonnee de la case.
+     * @return Element representant la case.
+     */
     public Element tile(int x, int y){
         if (x < 0 || x >= width || y < 0 || y >= height)
             return Element.BOUNDS;
@@ -47,16 +57,31 @@ public class World {
             return element[x][y];
     }
 
-
+    /**
+     * Methode retournant le caractere ASCII representant la case (x,y).
+     * @param x abscisse de la case.
+     * @param y ordonnee de la case.
+     * @return char representant le caractere ASCII.
+     */
 	public char glyph(int x, int y){
         return tile(x, y).getCaractere();
     }
 
 
+    /**
+     * Methode retournant la couleur de la case (x,y).
+     * @param x abscisse de la case.
+     * @param y ordonnee de la case.
+     * @return couleur de la case.
+     */
 	public Color color(int x, int y){
         return tile(x, y).getColor();
     }
 
+    /**
+     * Trouve des coordonnees pour la creature etant libre.
+     * @param creature a rajouter sur la map.
+     */
     public void addAtEmptyLocation(Creature creature){
         int x;
         int y;
@@ -75,10 +100,10 @@ public class World {
     {
     	return element[x][y];
     }
-    public Element[][] getElement()
+    /*public Element[][] getElement()
     {
     	return element;
-    }
+    }*/
     public void affectationElement(int x,int y,Element e)
     {
     	element[x][y]=e;
