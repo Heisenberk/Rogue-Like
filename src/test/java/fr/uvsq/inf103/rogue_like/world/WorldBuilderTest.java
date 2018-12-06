9d3c1819d4cb86340c4ff8e49ae4820ad1668fa0
Mehdi Merimi <mehdi.78340@hotmail.fr>
2018-12-06 13:29:33 +0100

    javadoc fichier test

--- src/test/java/fr/uvsq/inf103/rogue_like/world/WorldBuilderTest.java
+++ src/test/java/fr/uvsq/inf103/rogue_like/world/WorldBuilderTest.java
@@ -8,15 +8,24 @@ import org.junit.Test;
 public class WorldBuilderTest {
 
 	private WorldBuilder w;
+	/**
+	 * Permet d'initialiser les variables que l'on va tester. 
+	 */
 	@Before
 	public void initialize() {
 		w = new WorldBuilder(50,50);
 	}
+	/**
+	 * Tests si l'élément est pas null.
+	 */
 	@Test
 	public void testConstructeur() {
 		assertNotNull(w.getElement());
 		
 	}
+	/**
+	 * Tests si il existe de l'argent dans la map.
+	 */
 	@Test
 	public void testExistMoney() {
 		int nbmoney=0;
@@ -30,17 +39,20 @@ public class WorldBuilderTest {
 		assertNotEquals(nbmoney,0);
 	}
 	
+	/**
+	 * Tests si il existe une porte dans la map.
+	 */
 	@Test
 	public void testBuildWall() {
-		int nbmur=0;
-		w.WorldMakeMur(0, 0);
-		for(int i=0;i<3;i++) {
-			for (int j=0;j<3;j++) {
-				if(w.getPosition(i, j)==Element.WALL)
-				nbmur++;
+		int porte=0;
+		w.makeCaves();
+		for(int i=0;i<50;i++) {
+			for (int j=0;j<50;j++) {
+				if(w.getPosition(i, j)==Element.DOOR)
+				porte++;
 			}
 		}
-		assertNotEquals(nbmur,0);
+		assertNotEquals(porte,0);
 	}
 	
 
